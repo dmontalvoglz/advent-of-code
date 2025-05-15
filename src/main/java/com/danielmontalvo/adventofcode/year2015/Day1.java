0cd91e93418ba4da4f24f7fc03f8caeddc3d871b
dmontalvoglz <dmontalvoglz@gmail.com>
2025-05-15 06:14:42 -0600

    Completed the 1st part of 2015's 1st day successfully.

--- src/main/java/com/danielmontalvo/adventofcode/year2015/Day1.java
+++ src/main/java/com/danielmontalvo/adventofcode/year2015/Day1.java
@@ -8,9 +8,27 @@ public class Day1 {
     public static void main(String[] args) {
         try {
             List<String> lines = InputReader.readFileLines("year-2015/day-1-input-1.txt");
-            for (String line : lines) {
-                System.out.println(line);
+            if (lines.isEmpty()) {
+                System.err.println("No lines in file.");
+                return;
             }
+            // Get the first line and convert to an array of chars
+            String firstLine = lines.getFirst();
+            char[] characters = firstLine.toCharArray();
+            int currentFloor = 0;
+
+            // Checks the input and calculates the floor based on '(' or ')'
+            for (char c : characters) {
+                if (c == '(') {
+                    currentFloor++;
+                } else if (c == ')') {
+                    currentFloor--;
+                }
+            }
+
+            // Prints the result
+            System.out.println("The current floor is: " + currentFloor);
+
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
